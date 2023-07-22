17eee9dc51596ba4aae090f8cc88e4efa1ae4cb1
Salimov-dev <salimov.rent@mail.ru>
2023-07-23 02:49:00 +0300

    фикс бага с отображением юзеров

--- client/src/processes/hoc/app-loader.tsx
+++ client/src/processes/hoc/app-loader.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { loadNotesList } from "../../entities/note/store/notes-store";
 import { loadCommentsList } from "../../entities/comment/store/comments-store";
 import { loadCategoriesList } from "../../entities/categories/store/categories-store";
+import { loadUsersList } from "../../entities/user/store/users-store";
 
 interface AppLoaderProps {
   children: React.ReactNode;
@@ -16,6 +17,7 @@ const AppLoader = ({ children }: AppLoaderProps) => {
     dispatch<any>(loadNotesList());
     dispatch<any>(loadCommentsList());
     dispatch<any>(loadCategoriesList());
+    dispatch<any>(loadUsersList());
   }, []);
 
   return children;
